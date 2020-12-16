76fc489bd6cc4bc6964e8ffe745be921b237bd25
YiPing <pan.yiping.fi@gmail.com>
2020-12-16 22:34:48 +0800

    Remove unneeded ()

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,12 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do putStrLn "             ^  -  ^\n\
+                   \            ( .   . )\n\
+                   \              =>;<=\n\
+                   \             /     \\\n\
+                   \            |       |\n\
+                   \ * Rational Arithmetic Evaluation *\n\
+                   \   Example: (1%2) + (3%4) * (5%6)"
+          inputString <- getLine
+          putStrLn ("Your input is \"" ++ inputString ++ "\"!")
