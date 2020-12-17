fefa836e0fcecd01e07fc28aaeab9ac9c159cf08
YiPing <pan.yiping.fi@gmail.com>
2020-12-17 14:22:28 +0800

    New way implementing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
 module Main where
 
-import Lib
+import Data.Char
+
+data Operator = ADD | SUB | MUL | DIV | SPLIT
+    deriving (Show, Eq)
+
+data Parentesis = LBR | RBR
+    deriving (Show, Eq)
+
+data Token = TokOp Operator
+           | TokNum Int
+           | TokBR Parentesis
+    deriving (Show, Eq)
+
+operator :: Char -> Operator
+operator c | c == '+' = ADD
+           | c == '-' = SUB
+           | c == '*' = MUL
+           | c == '/' = DIV
+           | c == '%' = SPLIT
+
+parentesis :: Char -> Parentesis 
+parentesis c | c == '(' = LBR
+             | c == ')' = RBR
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize (c : cs) 
+    | elem c "+-*/%" = TokOp (operator c) : tokenize cs
+    | elem c "()" = TokBR (parentesis c) : tokenize cs   
+    | isDigit c  = TokNum (digitToInt c) : tokenize cs
+    | isSpace c  = tokenize cs
+    | otherwise  = error $ "Cannot tokenize " ++ [c]
+
 
 main :: IO ()
 main = do putStrLn "             ^  -  ^\n\
@@ -9,6 +41,6 @@ main = do putStrLn "             ^  -  ^\n\
                    \             /     \\\n\
                    \            |       |\n\
                    \ * Rational Arithmetic Evaluation *\n\
-                   \   Example: (1%2) + (3%4) * (5%6)"
+                   \   Example: (1%2) + (3%4) * (5%6) "
           inputString <- getLine
           putStrLn ("Your input is \"" ++ inputString ++ "\"!")
